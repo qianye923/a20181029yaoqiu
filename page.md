326df3b2d12f83499f0a7c3ee78ecd8c214d6a4b
v_chfli <vchfli923@gmail.com>
2018-10-29 20:53:08 +0800

    首页部分完完成

--- page.md
+++ page.md
@@ -1,2 +1,22 @@
 ###    wx.switchTab({  url: '' })只能跳转带有tabbar的页面;   wx.redirectTo({  url: '' })和wx.navigateTo({  url: '' })不能跳转tabbar绑定过的页面;  
 
+
+###   wx.showToast()  和wx.hideToast()
+
+
+
+## 页面数据请求
+
+### 01
+
+    <!-- 头部图片的轮播 -->
+       url:"https://api.it120.cc/tz/banner/list",
+       data: {
+        key: 'mallName'
+      },
+
+### 02
+
+
+  <!-- 导航条的链接 -->
+  url: 'https://api.it120.cc/' + app.globalData.subDomain + '/shop/goods/category/all',
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,53 @@
 Page({
   data:{
- 
+    indicatorDots:true,
+    interval:3000,
+    autoplay:true,
+    duration:2000,
+    bannerImg:[],
+    navBarLink:[]
   },
   onLoad:function(options){
+    var that=this;
     // 生命周期函数--监听页面加载
-  
+    //  01请求首页头图的数据
+    wx.request({
+       url:"https://api.it120.cc/tz/banner/list",
+       data: {
+        key: 'mallName'
+      },
+      success:function(res){
+        // console.log(res)
+        if(res.statusCode==200){
+           that.setData({
+             bannerImg:res.data.data
+           })
+        }else{
+           wx.showToast({
+             title:"没有数据O**0",
+             icon:"loading",
+             duration:"1500",
+             mask:"true"
+           })
+        }
+      }
+    })
+
+    // 02导航条的数据的请求
+
+    wx.request({
+      url:"https://api.it120.cc/tz/shop/goods/category/all",
+      success:function(res){
+         console.log(res.data.data)
+         if(res.statusCode==200){
+            that.setData({
+              navBarLink:res.data.data
+            })
+         }
+      }
+    })
+
+
   },
   onReady:function(){
     // 生命周期函数--监听页面初次渲染完成
